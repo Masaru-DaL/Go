40294ee994856740f00505c98d3cb9cd10253de2
Masaru <hittec9609@icloud.com>
2022-09-12 16:51:52 +0900

    Mercari

--- Investigation/IntoroductionToGolang_byMercari/Chapter.2/Omikuji/main.go
+++ Investigation/IntoroductionToGolang_byMercari/Chapter.2/Omikuji/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	
+	rand.Seed(time.Now().UnixNano())
+
+	Omikuji_num := rand.Intn(6)
+	switch Omikuji_num {
+	case 6:
+		fmt.Println("出た数字は", Omikuji_num, "! 大吉")
+	case 5, 4:
+		fmt.Println("出た数字は", Omikuji_num, "! 中吉")
+	case 3, 2:
+		fmt.Println("出た数字は", Omikuji_num, "! 吉")
+	case 1:
+		fmt.Println("出た数字は", Omikuji_num, "! 凶")
+	}
 }
