4c025d0c6ebfc4dee5f947cd55a303f1d2ebcee5
Masaru <hittec9609@icloud.com>
2022-09-30 13:12:59 +0900

    gqlgen

--- my-golang/gqlgen-tutorial/graph/resolver.go
+++ my-golang/gqlgen-tutorial/graph/resolver.go
@@ -1,7 +1,19 @@
 package graph
 
+import "gqlgen-tutorial/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	todos []*model.Todo
+}
+
+func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
+	todo := &model.Todo{
+		Text: input.Text,
+		ID: fmt.Sprintf("T%d", rand.Int()),
+		User:
+	}
+}
