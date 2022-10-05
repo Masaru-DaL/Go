c3c630a131120b50bfa444980e1e1a071e60ac5f
Masaru <hittec9609@icloud.com>
2022-10-05 16:38:11 +0900

    GraphQL Server

---
+++ src/github.com/graphql-tutorial/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"graphql-tutorial/graph"
+	"graphql-tutorial/graph/generated"
+	database "graphql-tutorial/internal/pkg/db/mysql"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/go-chi/chi"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	router := chi.NewRouter()
+
+	database.InitDB()
+	defer database.CloseDB()
+	database.Migrate()
+	server := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/query", server)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, router))
+}
