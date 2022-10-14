d25879c0b857f529d3ab2bea83898ee66f25afa7
Masaru <hittec9609@icloud.com>
2022-10-14 13:05:24 +0900

    docker-crud

--- my-golang/docker-crud/main.go
+++ my-golang/docker-crud/main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"dokcer-crud/todos"
 	"fmt"
 	"log"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+func status(c *fiber.Ctx) error {
+	return c.SendString("Server is running!! Send your request")
+}
+
+func setupRoutes(app *fiber.App) {
+
+	app.Get("/", status)
+
+	app.Get("/api/todos", todos.GetAllTasks)
+	app.Post("/api/todos", todos.SaveTask)
+}
 
 func main() {
 	app := fiber.New()
