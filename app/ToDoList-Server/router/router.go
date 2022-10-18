dcf511692944c12894b847c484d8f0175edc88a7
Masaru <hittec9609@icloud.com>
2022-10-18 11:32:41 +0900

    crud

--- app/ToDoList-Server/router/router.go
+++ app/ToDoList-Server/router/router.go
@@ -13,11 +13,14 @@ import (
 /* Routingを設定する関数 */
 // 引数はecho.echo型で、戻り値はerror型
 func SetRouter(e *echo.Echo) error {
+	// APIが叩かれた時にログを出す。
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${time_rfc3339_nano} ${host} ${method} ${uri} ${status} ${header}\n",
 		Output: os.Stdout,
 	}))
+	// 予想外のエラーが発生した際にもサーバを落とさないようにする
 	e.Use(middleware.Recover())
+	// CORSに対応する
 	e.Use(middleware.CORS())
 
 	/* APIを書く */
