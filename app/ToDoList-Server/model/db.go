860209b2a9319bac2237e319f027fea26c728ec6
Masaru <hittec9609@icloud.com>
2022-10-18 11:08:34 +0900

    crud

--- app/ToDoList-Server/model/db.go
+++ app/ToDoList-Server/model/db.go
@@ -12,38 +12,47 @@ import (
 
 var db *gorm.DB
 
-/* DB接続とテーブルを作成する */
+/* DBconnection: DB接続とテーブルを作成する関数 */
 func DBconnection() *sql.DB {
+	// GetDBConfigを実行し、戻り値をdsnと定義する
 	dsn := GetDBConfig()
+	// error型のerrを定義する
 	var err error
-	// Open関数でDB接続を行う
+	// dsnを使ってDBに接続する。
+	// 戻り値をdb, errに代入する
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(fmt.Errorf("DB Error: %w", err))
 	}
-	// テーブルの作成
+
+	// Task型のテーブルを作成する
 	CreateTable(db)
+	// *gorm.DB型を、*sql.DB型に変換する
 	sqlDB, err := db.DB()
 	if err != nil {
 		panic(fmt.Errorf("DB Error: %w", err))
 	}
+	// sqlDBを返す
 	return sqlDB
 }
 
-/* DBのdsnを取得する */
+/* DBのdsnを取得する関数 */
 func GetDBConfig() string {
-	/* 接続情報を設定 */
+	// docker-compose.ymlで設定した各種環境変数を読み込む
 	user := os.Getenv("DB_USERNAME")
 	password := os.Getenv("DB_PASSWORD")
 	hostname := os.Getenv("DB_HOSTNAME")
 	port := os.Getenv("DB_PORT")
 	dbname := os.Getenv("DB_DBNAME")
 
+	// dsn(DBの接続情報つける識別子)を定義する
 	dsn := fmt.Sprintf("%s.%s@tcp(%s:%s)/%s", user, password, hostname, port, dbname) + "?charset=utf8mb4&parseTime=True&loc=local"
+	// dsnを返す
 	return dsn
 }
 
-/* Task型のテーブルを作成する */
+/* Task型のテーブルを作成する関数 */
 func CreateTable(db *gorm.DB) {
+	// Task型のテーブルを作成する
 	db.AutoMigrate(&Task{})
 }
